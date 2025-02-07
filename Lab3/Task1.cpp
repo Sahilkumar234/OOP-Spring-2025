4aec5b5f22e44bda4c5079e132924063e85c3d35
Sahil <sahil7815kumar@gmail.com>
2025-02-07 12:48:40 +0500

    Update Task1.cpp

--- Lab3/Task1.cpp
+++ Lab3/Task1.cpp
@@ -1,1 +1,46 @@
 
+#include<iostream>
+using namespace std;
+
+class BMI {
+    private:
+    string status;
+    float weight;
+    float BMI;
+    float Height;
+    
+    public:
+    void setValue(string status,float weight,float BMI,float Height) {
+        this->status = status;
+        this->weight = weight;
+        this->BMI = BMI;
+        this->Height = Height;
+    }
+
+    void getInput() {
+        cout<<"Enter status: ";
+        cin>>status;
+        cout<<"Enter "
+    }
+
+
+    float calculateBMI(float weight,float Height) {
+        float BMI = weight /(Height*Height) * 703;
+        return BMI;
+    }
+    string findStatus(float BMI) {
+        string status;
+        if(BMI < 18.5) {
+            status = "underWeight";
+        }
+        else if(BMI < 25.0) {
+            status = "Normal";
+        }
+        return status;
+    }
+};
+
+int main() {
+
+    return 0;
+}
