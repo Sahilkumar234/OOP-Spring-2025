fd9acd015e2f12cec4139b50ae400960de46734a
Sahil <sahil7815kumar@gmail.com>
2025-04-22 19:24:29 +0500

    Create Course.h

---
+++ Lab 09/Course.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include<iostream>
+using namespace std;
+
+class Course
+{
+protected:
+    string courseCode;
+    int credits;
+public:
+    Course(string courseCode, int credits) : courseCode(courseCode), credits(credits) {}
+
+    virtual void calculateGrade() = 0;
+    virtual void displayInfo() = 0;
+    virtual ~Course() {}
+};
